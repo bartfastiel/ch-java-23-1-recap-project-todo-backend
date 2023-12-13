c2abc5d525b9ef831d87b0c3ab4a77f5027ec3ce
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-12-13 05:12:07 +0100

    Setup project (spring web+lombok)

---
+++ src/main/java/com/example/chjava231recapprojecttodobackend/ChJava231RecapProjectTodoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.chjava231recapprojecttodobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChJava231RecapProjectTodoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChJava231RecapProjectTodoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chjava231recapprojecttodobackend/ChJava231RecapProjectTodoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chjava231recapprojecttodobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChJava231RecapProjectTodoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
